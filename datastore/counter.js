475758c5c06152c3e1e01a8e968dd4b45a0ba96b
davidguy3237 <davidtran3237@gmail.com>
2023-04-17 14:51:12 -0700

    Complete getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const sprintf = require('sprintf-js').sprintf;
 
-var counter = 0;
-
 // Private helper functions ////////////////////////////////////////////////////
 
 // Zero padded numbers can only be represented as strings.
@@ -38,9 +36,17 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+
+  readCounter((err, count) => {
+    if (err) {
+      console.log('READ COUNTER ERROR');
+    } else {
+      writeCounter(count + 1, (err, counterString) => {
+        callback(err, counterString);
+      });
+    }
+  });
 };
 
 
